4d7b03bada53ff8beead8d6f9b1910ad32cd0ccf
shellieD <shellie.downie13@gmail.com>
2022-01-05 17:49:02 +0000

    Add if/elif/else statement to handle user response

--- run.py
+++ run.py
@@ -3,7 +3,8 @@ import emoji
 
 def start_game():
     """
-    Opens the title screen and
+    Opens the title screen, requests players name and asks if player
+    would like to see view rules and story.
     """
     print("\U0001f4a9" * 26)
     result = pyfiglet.figlet_format("DONT STEP\nIN THE\nPOOP")
@@ -13,4 +14,19 @@ def start_game():
     name = input("Please enter your name: ")
     rules_response = input("Hi " + name + ". Would you like to see the game rules & backstory? Enter y/n \n").lower()
 
-    
+    if rules_response == 'y':
+        print("game_rules")
+    elif rules_response == 'n':
+        ready_to_play = input("Are you ready to play? y/n \n").lower()
+        if ready_to_play == 'y':
+                print("open_game_board")
+        if ready_to_play == 'n':
+            print("You said NO!  Goodbye for now.\n")
+            print("\U0001f4a9" * 22)
+            result = pyfiglet.figlet_format("GOODBYE")
+            print(result)
+            print("\U0001f4a9" * 22)
+    else:
+        print("You have made an incorrect selection. Please try again\n")
+
+start_game()
