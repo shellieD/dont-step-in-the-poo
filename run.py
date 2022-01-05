4b2ce4803f2d58294caf5d392fac54d59ad9f18c
shellieD <shellie.downie13@gmail.com>
2022-01-05 15:43:47 +0000

    Add start_game function to display title screen

--- run.py
+++ run.py
@@ -1,3 +1,22 @@
 import pyfiglet
 import emoji
 
+def start_game():
+    """
+    Opens the title screen, requests players name and asks if player
+    would like to see view rules and story.
+    """
+    print("\U0001f4a9 \U0001f4a9 \U0001f4a9 \U0001f4a9 \U0001f4a9 \U0001f4a9 \U0001f4a9"
+    "\U0001f4a9 \U0001f4a9 \U0001f4a9 \U0001f4a9 \U0001f4a9 \U0001f4a9 \U0001f4a9"
+    "\U0001f4a9 \U0001f4a9 \U0001f4a9 \U0001f4a9")
+    result = pyfiglet.figlet_format("DONT STEP\nIN THE\nPOOP")
+    print(result)
+    print("\U0001f4a9 \U0001f4a9 \U0001f4a9 \U0001f4a9 \U0001f4a9 \U0001f4a9 \U0001f4a9"
+    "\U0001f4a9 \U0001f4a9 \U0001f4a9 \U0001f4a9 \U0001f4a9 \U0001f4a9 \U0001f4a9"
+    "\U0001f4a9 \U0001f4a9 \U0001f4a9 \U0001f4a9\n"
+    )
+    
+    name = input("Please enter your name: ")
+    rules_response = input("Hi " + name + ". Would you like to see the game rules & backstory? Enter y/n \n").lower()
+
+start_game()
