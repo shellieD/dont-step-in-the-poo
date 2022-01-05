950cc6e6c3888ad5a61301163932dbf1b2a21ce6
shellieD <shellie.downie13@gmail.com>
2022-01-05 16:03:41 +0000

    Add function to validate users response.

--- run.py
+++ run.py
@@ -15,8 +15,27 @@ def start_game():
     "\U0001f4a9 \U0001f4a9 \U0001f4a9 \U0001f4a9 \U0001f4a9 \U0001f4a9 \U0001f4a9"
     "\U0001f4a9 \U0001f4a9 \U0001f4a9 \U0001f4a9\n"
     )
-    
-    name = input("Please enter your name: ")
-    rules_response = input("Hi " + name + ". Would you like to see the game rules & backstory? Enter y/n \n").lower()
+
+    while True:
+        name = input("Please enter your name: ")
+        rules_response = input("Hi " + name + ". Would you like to see the game rules & backstory? Enter y/n \n").lower()
+
+        validate_response(rules_response)
+
+    return rules_response
+
+def validate_response(rules_response):  
+    if rules_response == 'y':
+        print("GAME RULES")
+    elif rules_response == 'n':
+        ready_to_play = input("Are you ready to play? y/n \n").lower()
+        if ready_to_play == 'y':
+            print("SHOW GAME BOARD")
+        if ready_to_play == 'n':
+            print("You said NO!  Goodbye for now.")
+    else:
+        print("You have made an incorrect selection. Please try again\n")
+        input("Press Enter to continue...")
+    return True
 
 start_game()
