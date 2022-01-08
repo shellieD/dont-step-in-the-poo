fb2536a8abc1197985ae3914fac80811a188ab4b
shellieD <shellie.downie13@gmail.com>
2022-01-08 01:58:23 +0000

    Add function to validate input for user guess

--- test.py
+++ test.py
@@ -1,21 +1,63 @@
 from random import randrange
 
 
-clear_path = randrange(0,8)
-print(clear_path)
+def set_up_game():
+    """
+    Selects a random row as a clear path through the board.
+    Distributes a poo randomly to each other row.
+    """
 
-poos = []
-for i in range(8):
-    if i != clear_path:
-        poos.append((i, randrange(0, 8)))
-print(poos)
+    clear_path = randrange(0, 8)
+    print(clear_path)
 
-row = int(input("choose row"))
-column = int(input("choose column"))
+    poos = []
+    for i in range(8):
+        if i != clear_path:
+            poos.append((i, randrange(0, 8)))
+    print(poos)
 
-coord = (row, column)
+    while True:
+        row = int(input("Choose a row \n"))
+        validate_input(row)
 
-if coord in poos:
-    print("What a mess!!!")
-else:
-    print("OOooh that was a close one!") 
+        if validate_input(row):
+            print("data is valid")
+            break
+
+    while True:
+        column = int(input("Choose a column \n"))
+        validate_input(column)
+
+        if validate_input(column):
+            print("data is valid")
+            break
+
+        coord = (row, column)
+        player_guess = []
+
+        if coord in poos:
+            print("What a mess!!!")
+        else:
+            print("Phew, no poo there!!")
+            player_guess.append(coord)
+            print(player_guess)
+
+
+def validate_input(value):
+    """
+    Checks that the value entered is within
+    the range 0-7
+    """
+    try:
+        if value not in range(0, 7):
+            raise ValueError(
+                f"Please choose a number between 0 and 7. You entered: {value}"
+            )
+    except ValueError as e:
+        print(f"Invalid Data {e}. Please try again. ")
+        return False
+
+    return True
+
+
+set_up_game()
